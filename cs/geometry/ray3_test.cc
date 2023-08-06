4223e62a2cdfcc29e9f2ce871b22a18fdf8ccf43
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-06 16:28:45 +0000

    ray direction tests

--- cs/geometry/ray3_test.cc
+++ cs/geometry/ray3_test.cc
@@ -7,4 +7,21 @@ using p3 = ::cs::geometry::Point3;
 using v3 = ::cs::geometry::Vector3;
 using r3 = ::cs::geometry::Ray3;
 
-TEST(Ray3, Ok) { EXPECT_TRUE(true); }
+#define EXPECT_POINT3_EQ(a, b) \
+  EXPECT_FLOAT_EQ(a.x, b.x);   \
+  EXPECT_FLOAT_EQ(a.y, b.y);   \
+  EXPECT_FLOAT_EQ(a.z, b.z);
+
+#define EXPECT_VECTOR3_EQ(vecA, vecB) \
+  EXPECT_POINT3_EQ(vecA.a, vecB.a);   \
+  EXPECT_POINT3_EQ(vecA.b, vecB.b);
+
+TEST(Ray3, DirectionVectorIsUnit) {
+  r3 ray(p3(), p3(1, 1, 1));
+  EXPECT_VECTOR3_EQ(ray.direction, ray.direction.unit());
+  EXPECT_POINT3_EQ(ray.direction.a, p3());
+  EXPECT_POINT3_EQ(ray.direction.b,
+                   p3(0.57735026918962576450914878050196,
+                      0.57735026918962576450914878050196,
+                      0.57735026918962576450914878050196));
+}
