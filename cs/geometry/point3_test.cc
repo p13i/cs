77763312cf5e93fe935759350ea52fc4a647acb8
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-06 00:23:33 +0000

    simplify test

--- cs/geometry/point3_test.cc
+++ cs/geometry/point3_test.cc
@@ -1,13 +1,10 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include "cs/geometry/point3.h"
 
-#include "cs/geometry/point3.h" 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 
 using p3 = ::cs::geometry::Point3;
 
 TEST(Point3, OperatorAdd) {
-    p3 a{1, 2, 3};
-    p3 b{4, 5, 6};
-    p3 expected{5,7,9};
-    EXPECT_EQ(a + b, expected);
+  EXPECT_EQ(p3(1, 2, 3) + p3(4, 5, 6), p3(5, 7, 9));
 }
