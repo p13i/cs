2bd438c4476120843d53db532031f45afbe1a0bd
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-06 20:50:36 -0700

    remove nolint

--- cs/string/format.h
+++ cs/string/format.h
@@ -14,12 +14,10 @@ template <typename... Args>
 std::string format(const std::string &format,
                    Args... args) {
   // Extra space for '\0' so plus 1
-  // NOLINTNEXTLINE(format-security)
   int size =
       std::snprintf(nullptr, 0, format.c_str(), args...) +
       1;
   char *buf = new char[static_cast<size_t>(size)]();
-  // NOLINTNEXTLINE(format-security)
   std::snprintf(buf, size, format.c_str(), args...);
   // We don't want the '\0' inside so subtract 1
   return {buf, buf + size - 1};
