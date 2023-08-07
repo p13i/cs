b6f588f9bbcde2915db1d265a692e0e2a00d2511
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-07 03:12:30 +0000

    add random

---
+++ cs/math/random.cc
@@ -0,0 +1,7 @@
+#include "random.h"
+
+#include <random>
+
+float cs::math::randomFloat() {
+  return std::rand() / static_cast<float>(RAND_MAX);
+};
---
+++ cs/math/random.h
@@ -0,0 +1,8 @@
+#ifndef CS_MATH_RANDOM_H
+#define CS_MATH_RANDOM_H
+
+namespace cs::math {
+float randomFloat();
+}  // namespace cs::math
+
+#endif  // CS_MATH_RANDOM_H
