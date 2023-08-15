7fd565f6b6b17ac0ed35bb6a27507b73a1d145ea
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-15 20:16:44 +0000

    add dot impl for vectors and sanity check

--- cs/geo/dot.cc
+++ cs/geo/dot.cc
@@ -1,8 +1,26 @@
 #include "cs/geo/dot.hh"
 
+#include "cs/geo/point3.h"
+#include "cs/geo/vector3.h"
+#include "cs/math/constants.h"
+#include "cs/sanity/ensure.hh"
+
+using p3 = ::cs::geo::Point3;
+using v3 = ::cs::geo::Vector3;
+
 namespace cs::geo {
 // Performs a standard dot product.
-float dot(const Point3& a, const Point3& b) {
+float dot(const p3& a, const p3& b) {
   return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+float dot(const v3& a, const v3& b) {
+  // Move both vectors to origin
+  v3 vec_a = a - a.a;
+  v3 vec_b = b - b.a;
+  ENSURE(vec_a.a == p3());
+  ENSURE(vec_b.a == p3());
+  // Call the point implementation above
+  return dot(vec_a.b, vec_b.b);
 }
 }  // namespace cs::geo
--- cs/geo/dot.hh
+++ cs/geo/dot.hh
@@ -2,10 +2,15 @@
 #define CS_GEO_DOT_H
 
 #include "cs/geo/point3.h"
+#include "cs/geo/vector3.h"
+
+using p3 = ::cs::geo::Point3;
+using v3 = ::cs::geo::Vector3;
 
 namespace cs::geo {
 // Performs a standard dot product.
-float dot(const Point3& a, const Point3& b);
+float dot(const p3& a, const p3& b);
+float dot(const v3& a, const v3& b);
 }  // namespace cs::geo
 
 #endif  // CS_GEO_DOT_H
