e3bf12a6515fdfca2e5432e14784a6d818706dfe
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-17 06:21:51 +0000

    map normal dot prod to luminance

--- cs/renderer/scene_renderer.hh
+++ cs/renderer/scene_renderer.hh
@@ -10,6 +10,7 @@
 #include "cs/geo/ray3.h"
 #include "cs/geo/vector3.h"
 #include "cs/math/map_value.hh"
+#include "cs/numbers/clamp.hh"
 #include "cs/renderer/film.hh"
 #include "cs/renderer/scene.hh"
 #include "cs/sanity/ensure.hh"
@@ -20,6 +21,7 @@ using v3 = ::cs::geo::Vector3;
 using r3 = ::cs::geo::Ray3;
 using ::cs::geo::dist;
 using ::cs::math::map_value;
+using ::cs::numbers::clamp;
 using ::cs::renderer::Film;
 using ::cs::shapes::Sphere;
 
@@ -37,9 +39,6 @@ class SceneRenderer {
                                /*radius=*/0.5)});
 
   Film render() {
-    float min_distance = 3;
-    float max_distance = 7;
-
     float x_units = film_.width / pixels_per_unit_;
     float y_units = film_.height / pixels_per_unit_;
     float film_x_start = film_center_.x - x_units / 2.f;
@@ -47,11 +46,11 @@ class SceneRenderer {
     float film_y_start = film_center_.y + y_units / 2.f;
     float film_y_end = film_center_.y - y_units / 2.f;
     for (uint32_t fx = 0; fx < film_.width; fx++) {
-      float real_x = map_value(fx, 0, film_.width,
-                               film_x_start, film_x_end);
+      float real_x = map_value<float>(
+          fx, 0, film_.width, film_x_start, film_x_end);
       for (uint32_t fy = 0; fy < film_.height; fy++) {
-        float real_y = map_value(fy, 0, film_.height,
-                                 film_y_start, film_y_end);
+        float real_y = map_value<float>(
+            fy, 0, film_.height, film_y_start, film_y_end);
         float real_z = film_center_.z;
         r3 ray(focal_point_, p3(real_x, real_y, real_z));
 
@@ -59,22 +58,13 @@ class SceneRenderer {
         v3 normal;
         if (scene_.intersectedBy(ray, &intersection_point,
                                  &normal)) {
-          float distance =
-              cs::geo::dist(ray.origin, intersection_point);
-#if 0
-          if (distance > max_distance) {
-            max_distance = distance;
-            std::cout
-                << "Film point fx=" << fx << ",fy=" << fx
-                << " max_distance=" << max_distance << "\n";
-          }
-#endif
-          ENSURE(distance >= min_distance);
-          ENSURE(distance <= max_distance);
-          float rgb = map_value(distance, min_distance,
-                                max_distance, 255, 255/3);
-          ENSURE(0 <= rgb && rgb <= 255.f);
-          film_.pixels[fx][fy] = Pixel(rgb, rgb, rgb, 255);
+          float unit_dot_prod = dot(
+              v3(intersection_point, film_center_).unit(),
+              normal.unit());
+          float luminance =
+              map_value<float>(unit_dot_prod, 0, 1, 0, 255);
+          film_.pixels[fx][fy] =
+              Pixel(luminance, luminance, luminance, 255);
         } else {
           film_.pixels[fx][fy] = Pixel(0, 0, 0, 0);
         }
