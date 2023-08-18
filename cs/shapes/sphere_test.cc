403c414eb7b6e850b8aa4a8d81de894350f012b6
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-17 23:36:21 -0700

    Update sphere_test.cc

--- cs/shapes/sphere_test.cc
+++ cs/shapes/sphere_test.cc
@@ -8,15 +8,6 @@ using p3 = ::cs::geo::Point3;
 using v3 = ::cs::geo::Vector3;
 using r3 = ::cs::geo::Ray3;
 
-#define EXPECT_POINT3_EQ(a, b)          \
-  EXPECT_NEAR(a.x, b.x, FLOAT_EPSILON); \
-  EXPECT_NEAR(a.y, b.y, FLOAT_EPSILON); \
-  EXPECT_NEAR(a.z, b.z, FLOAT_EPSILON);
-
-#define EXPECT_VECTOR3_EQ(A, B) \
-  EXPECT_POINT3_EQ(A.a, B.a);   \
-  EXPECT_POINT3_EQ(A.b, B.b);
-
 TEST(Sphere, IntersectionOnXAxis) {
   r3 ray(p3(0, 0, 0), p3(1, 0, 0));
   cs::shapes::Sphere sphere(p3(5, 0, 0), 0.5);
