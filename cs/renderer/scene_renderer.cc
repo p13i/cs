cf02bdb3b57f6432ca40611e8b33304cc8e7392d
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-08-20 08:21:36 +0000

    refactor/comment renderer code

--- cs/renderer/scene_renderer.cc
+++ cs/renderer/scene_renderer.cc
@@ -26,34 +26,37 @@ using ::cs::renderer::Film;
 using ::cs::shapes::Sphere;
 
 Film cs::renderer::SceneRenderer::render() {
+  // Unpack dimensions
   auto [width, height] = camera_.film_.dimensions();
   float x_units = width / camera_.pixels_per_unit_;
   float y_units = height / camera_.pixels_per_unit_;
-  p3 film_start = camera_.film_center_ +
+  // Unpack freqently-used variables
+  p3 film_center = camera_.film_center_;
+  // Compute bounds of film plane to sample
+  p3 film_start = film_center +
                   p3(-1 * x_units / 2.f, y_units / 2.f, 0);
-  p3 film_end = camera_.film_center_ +
+  p3 film_end = film_center +
                 p3(x_units / 2.f, -1 * y_units / 2.f, 0);
+  // Compute each pixel value
   for (unsigned int fx = 0; fx < width; fx++) {
     for (unsigned int fy = 0; fy < height; fy++) {
+      // Compute ray projected from focal point through film
+      // point
       p3 real_p = map_value<p3>(p3(fx, fy, 0), p3(0, 0, 0),
                                 p3(width, height, 0),
                                 film_start, film_end);
-      real_p.z = camera_.film_center_.z;
+      real_p.z = film_center.z;
       r3 ray(camera_.focal_point_, real_p);
 
-#if 0
-        std::cout << "Ray at " << Tuple(fx, fy) << " is "
-                  << ray << std::endl;
-#endif
-
+      // Find intersection from this pixel
       p3 intersection_point;
       v3 normal;
       if (scene_.intersected_by(ray, &intersection_point,
                                 &normal)) {
-        // Compute the luminance of a pixel with an intersection
+        // Compute the luminance of a pixel with an
+        // intersection
         float unit_dot_prod =
-            dot(v3(intersection_point, camera_.film_center_)
-                    .unit(),
+            dot(v3(intersection_point, film_center).unit(),
                 normal.unit());
         float luminance =
             map_value<float>(unit_dot_prod, 0, 1, 0, 255);
