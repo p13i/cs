d64fa421fbb4035327f2955d787c512e95844c2a
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-10-25 05:34:56 +0000

    adjust a font

--- cs/app/text/draw_to_console.cc
+++ cs/app/text/draw_to_console.cc
@@ -7,14 +7,14 @@ using ::cs::app::text::fonts::mono;
 int main() {
   // Print each character and it's associated mono font
   // drawn from an array
-  for (int i = 0; i < 26; i++) {
+  for (int i = 0; i < 1; i++) {
     std::cout << (char)(i + 97) << " (index=" << i
               << "):" << std::endl;
     for (int y = 0; y < 8; y++) {
       for (int x = 0; x < 8; x++) {
         int flag = mono[i][y][x];
         if (flag) {
-          std::cout << "o";
+          std::cout << ".";
         } else {
           std::cout << " ";
         }
