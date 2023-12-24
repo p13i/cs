5f7a61e6539878affeca70e07b38d055dfdbeeeb
Pramod Kotipalli <p13i@users.noreply.github.com>
2023-12-24 05:26:52 +0000

    abort on bad map value

--- cs/numbers/map_value_test.cc
+++ cs/numbers/map_value_test.cc
@@ -20,17 +20,3 @@ TEST(MapValueTest, TestInRange) {
 
   EXPECT_TRUE(FloatsNear(mapped_value, 105.0f));
 }
-
-TEST(MapValueTest, TestOutOfRange) {
-  float value = 150.0f;
-  float value_range_start = 0.0f;
-  float value_range_end = 100.0f;
-  float new_range_start = 10.0f;
-  float new_range_end = 200.0f;
-
-  float mapped_value =
-      map_value(value, value_range_start, value_range_end,
-                new_range_start, new_range_end);
-
-  EXPECT_TRUE(FloatsNear(mapped_value, 295));
-}
