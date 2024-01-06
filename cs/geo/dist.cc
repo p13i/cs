410ae9e83b0b8b0a73878585b9fd352da4e53ab5
Pramod Kotipalli <p13i@users.noreply.github.com>
2024-01-06 08:47:36 -0800

    Update dist.cc

--- cs/geo/dist.cc
+++ cs/geo/dist.cc
@@ -5,8 +5,10 @@ namespace cs::geo {
 
 float dist(p3 a, p3 b) {
   p3 diff = b - a;
-  return sqrtf(diff.x * diff.x + diff.y * diff.y +
-               diff.z * diff.z);
+  float x2 = diff.x * diff.x;
+  float y2 = diff.y * diff.y;
+  float z2 = diff.z * diff.z;
+  return sqrtf(x2 + y2 + z2);
 }
 
 }  // namespace cs::geo
