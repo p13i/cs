1e6237b68e817f370f120a3140de865505aaecb0
Pramod Kotipalli <p13i@users.noreply.github.com>
2024-02-13 07:52:01 +0000

    float test class

--- cs/net/json/object_test.cc
+++ cs/net/json/object_test.cc
@@ -12,10 +12,10 @@ using ::cs::net::json::Type;
 using ::cs::result::Result;
 using ::testing::Eq;
 using ::testing::FloatEq;
-using ::testing::StrEq;
 using ::testing::IsFalse;
 using ::testing::IsTrue;
 using ::testing::Matcher;
+using ::testing::StrEq;
 
 class ParseTest : public ::testing::Test {
  public:
@@ -59,79 +59,77 @@ TEST_F(ParseBooleanTest, FalseWithStuffAfter) {
   EXPECT_THAT(result.data(), IsFalse());
 }
 
-TEST_F(ParseBooleanTest, TrueWithWhitespaceBeforeShouldError) {
+TEST_F(ParseBooleanTest,
+       TrueWithWhitespaceBeforeShouldError) {
   auto result = ParseBoolean(" false", &_cursor);
   EXPECT_THAT(result.ok(), IsFalse());
   EXPECT_THAT(_cursor, Eq(0));
 }
 
-TEST(ParseFloat, Float0) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("0", &cursor).value(), FloatEq(0));
+class ParseFloatTest : public ParseTest {};
+
+TEST_F(ParseFloatTest, Float0) {
+  EXPECT_THAT(ParseFloat("0", &_cursor).value(), FloatEq(0));
 }
 
-TEST(ParseFloat, Float0_1) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("0.1", &cursor).value(),
+TEST_F(ParseFloatTest, Float0_1) {
+  EXPECT_THAT(ParseFloat("0.1", &_cursor).value(),
               FloatEq(0.1));
 }
 
-TEST(ParseFloat, Float1_1) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("1.1", &cursor).value(),
+TEST_F(ParseFloatTest, Float1_1) {
+  EXPECT_THAT(ParseFloat("1.1", &_cursor).value(),
               FloatEq(1.1));
 }
 
-TEST(ParseFloat, FloatNeg0) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("-0", &cursor).value(),
+TEST_F(ParseFloatTest, FloatNeg0) {
+  EXPECT_THAT(ParseFloat("-0", &_cursor).value(),
               FloatEq(0));
 }
 
-TEST(ParseFloat, FloatPos0) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("+0", &cursor).value(),
+TEST_F(ParseFloatTest, FloatPos0) {
+  EXPECT_THAT(ParseFloat("+0", &_cursor).value(),
               FloatEq(0));
 }
 
-TEST(ParseFloat, Float100) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("100", &cursor).value(),
+TEST_F(ParseFloatTest, Float100) {
+  EXPECT_THAT(ParseFloat("100", &_cursor).value(),
               FloatEq(100));
 }
 
-TEST(ParseFloat, Float100_001) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("100.001", &cursor).value(),
+TEST_F(ParseFloatTest, Float100_001) {
+  EXPECT_THAT(ParseFloat("100.001", &_cursor).value(),
               FloatEq(100.001));
 }
 
-TEST(ParseFloat, Float1e1) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("1e0", &cursor).value(),
+TEST_F(ParseFloatTest, Float1e1) {
+  EXPECT_THAT(ParseFloat("1e0", &_cursor).value(),
               FloatEq(1));
 }
 
-TEST(ParseFloat, Float12e2) {
-  uint cursor = 0;
-  EXPECT_THAT(ParseFloat("12e2", &cursor).value(),
+TEST_F(ParseFloatTest, Float12e2) {
+  EXPECT_THAT(ParseFloat("12e2", &_cursor).value(),
               FloatEq(1200));
 }
 
 class ParseStringTest : public ParseTest {};
 
 TEST_F(ParseStringTest, EmptyString) {
-  EXPECT_THAT(*ParseString("\"\"", &_cursor).value(), StrEq(""));
+  EXPECT_THAT(*ParseString("\"\"", &_cursor).value(),
+              StrEq(""));
 }
 
 TEST_F(ParseStringTest, abc) {
-  EXPECT_THAT(*ParseString("\"abc\"", &_cursor).value(), StrEq("abc"));
+  EXPECT_THAT(*ParseString("\"abc\"", &_cursor).value(),
+              StrEq("abc"));
 }
 
 TEST_F(ParseStringTest, abcWithSpaces) {
-  EXPECT_THAT(*ParseString("\" a b c \"", &_cursor).value(), StrEq(" a b c "));
+  EXPECT_THAT(*ParseString("\" a b c \"", &_cursor).value(),
+              StrEq(" a b c "));
 }
 
 TEST_F(ParseStringTest, Newline) {
-  EXPECT_THAT(*ParseString("\"\n\"", &_cursor).value(), StrEq("\n"));
+  EXPECT_THAT(*ParseString("\"\n\"", &_cursor).value(),
+              StrEq("\n"));
 }
