fdfd1d8fd38833ff83b0fa9ef8924e70baed5938
Pramod Kotipalli <p13i@users.noreply.github.com>
2024-03-11 04:45:30 +0000

    comments

--- cs/db/main.cc
+++ cs/db/main.cc
@@ -6,11 +6,16 @@
 #include "cs/db/table.hh"
 
 int main() {
+  // Fake data.
   std::vector<User> users{
       {1, "Email", "Name"},
       {2, "Email2", "Name2"},
   };
+
+  // Build a table.
   auto table = Table<User>(users);
+
+  // Query the table.
   auto view = table.query_view()
                   .Where([](User u) {
                     return u.full_name().size() > 1;
