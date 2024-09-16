6ce518641945dfc58f0ad690ef048a94a8cb4d90
Pramod Kotipalli <p13i@users.noreply.github.com>
2024-09-16 04:11:35 +0000

    control camera perspective with arrow keys

--- cs/app/index.cc
+++ cs/app/index.cc
@@ -27,6 +27,9 @@ using ::cs::app::SceneAnimator;
 using ::cs::renderer::Film;
 using ::cs::renderer::Pixel;
 
+static p3 pos(-25, 0, -10);
+static p3 look(0, 0, 0);
+
 #ifdef __EMSCRIPTEN__
 // Callback function to handle keydown events
 EM_BOOL key_callback(int eventType,
@@ -34,6 +37,23 @@ EM_BOOL key_callback(int eventType,
                      void* userData) {
   printf("Key pressed: %s\n",
          e->key);  // Print the key pressed
+  p3 new_pos = pos;
+  if (strcmp(e->key, "ArrowLeft") == 0) {
+    new_pos.x -= 1;
+  } else if (strcmp(e->key, "ArrowRight") == 0) {
+    new_pos.x += 1;
+  } else if (strcmp(e->key, "ArrowUp") == 0) {
+    new_pos.y += 1;
+  } else if (strcmp(e->key, "ArrowDown") == 0) {
+    new_pos.y -= 1;
+  } else if (strcmp(e->key, "=") == 0) {
+    // move towards look
+    new_pos = (pos - look) * 0.8;
+  } else if (strcmp(e->key, "-") == 0) {
+    // move away from look
+    new_pos = (pos - look) * 1.2;
+  }
+  pos = new_pos;
   return EM_TRUE;  // Return EM_TRUE to prevent the default
                    // browser behavior
 }
@@ -61,10 +81,30 @@ int main(int argc, char** argv) {
 
   std::tuple<unsigned int, unsigned int> film_dimensions(
       APP_SCREEN_WIDTH, APP_SCREEN_HEIGHT);
-  SceneAnimator animator(APP_ANIMATION_NUM_FRAMES,
-                         film_dimensions);
 
-  std::vector<Film> frames;
+  // Setup scene
+  std::vector<Shape*> shapes{
+      // Unit sphere at the origin
+      new Sphere(p3(0, 0, 0), 0.5),
+      // One smaller sphere at x=-1
+      new Sphere(p3(-1, 0, 0), 0.25),
+      // One even smaller sphere at y=1
+      new Sphere(p3(0, 1, 0), 0.125),
+      // One even smaller sphere at z=-1
+      new Sphere(p3(0, 0, -1), 0.0625),
+      // new Sphere(p3(2, 0, 0), 0.5),
+      // new Sphere(p3(0, 2, 0), 0.25),
+      // new Sphere(p3(0, -1, 0), 0.25),
+      // new Plane(p3(1, 1, 1).unit(), -5),
+  };
+
+  Scene scene(shapes);
+
+  // Setup camera
+  unsigned int pixels_per_unit =
+      std::min(std::get<0>(film_dimensions),
+               std::get<1>(film_dimensions)) /
+      2;
 
   std::cout << "Rendering " << APP_ANIMATION_NUM_FRAMES
             << " frames with resolution <"
@@ -76,33 +116,21 @@ int main(int argc, char** argv) {
   emscripten_sleep(0);
 #endif  // __EMSCRIPTEN__
 
-  unsigned int render_time_ms =
-      time_it([&frames, &animator]() {
-        frames = animator.render_all_frames(
-            [](unsigned int render_time_ms,
-               unsigned int frame_num,
-               unsigned int num_frames) {
-              std::cout << "Rendered frame " << frame_num
-                        << " of " << num_frames << " in "
-                        << render_time_ms << " ms."
-                        << "\n";
-#ifdef __EMSCRIPTEN__
-              emscripten_sleep(0);
-#endif  // __EMSCRIPTEN__
-            });
-      });
-
-  std::cout << "done in " << render_time_ms << " ms!"
-            << "\n";
-
-  size_t frame_i = 0;
   while (true) {
     if (SDL_MUSTLOCK(screen)) {
       SDL_LockSurface(screen);
     }
 
     // Copy each pixel from the current animation frame
-    Film film = frames[frame_i];
+    p3 up(0, 1, 0);
+    Transform w2c = LookAt(pos, look, up);
+    float focal_length = 5;
+    Camera camera(w2c, pixels_per_unit, focal_length,
+                  Film(film_dimensions));
+
+    // Setup renderer
+    SceneRenderer renderer(camera, scene);
+    Film film = renderer.render();
     for (uint32_t i = 0; i < film.width; i++) {
       for (uint32_t j = 0; j < film.height; j++) {
         Pixel pixel = film.pixels[i][j];
@@ -122,9 +150,6 @@ int main(int argc, char** argv) {
 #ifdef __EMSCRIPTEN__
     emscripten_sleep(1000 / APP_FRAME_RATE_FPS);
 #endif  // __EMSCRIPTEN__
-
-    // Loop the animation
-    frame_i = (frame_i + 1) % APP_ANIMATION_NUM_FRAMES;
   }
 
   SDL_Quit();
