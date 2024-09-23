80067f36049cfd578ddd20a9228c59f5551c7924
Pramod Kotipalli <p13i@users.noreply.github.com>
2024-09-23 06:07:09 +0000

    improve draw to console

--- cs/app/text/draw_to_console.cc
+++ cs/app/text/draw_to_console.cc
@@ -7,17 +7,18 @@ using ::cs::app::text::fonts::mono;
 int main() {
   // Print each character and it's associated mono font
   // drawn from an array
-  for (int i = 0; i < 2; i++) {
+  for (uint i = 0; i < sizeof(mono); i++) {
     std::cout << (char)(i + 'A') << " (index=" << i << "):"
               << "\n";
     for (int y = 0; y < 8; y++) {
       for (int x = 0; x < 8; x++) {
         int flag = mono[i][y][x];
         if (flag) {
-          std::cout << ".";
+          std::cout << "o";
         } else {
           std::cout << " ";
         }
+        std::cout << " ";
       }
       std::cout << "\n";
     }
